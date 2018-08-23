911261e7bf9ee7c80bddbd5ac8b687d7a5d74cb3
Edmundo Pichardo <ce.pichardo@gmail.com>
2018-08-23 10:09:45 -0400

    add Category Properties and relations

--- app/Category.php
+++ app/Category.php
@@ -6,8 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
+
+    protected  $fillable =['slug','name','description'];
+
+    public function parent()
+    {
+        return $this->belongsTo(Category::class,'parent_id');
+    }
+
     public function children()
     {
-        return $this->hasMany(Category::class);
+        return $this->hasMany(Category::class,'parent_id');
     }
 }
