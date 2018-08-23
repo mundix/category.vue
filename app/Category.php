6f124cb9057e7bf59bca44fd4853f972288d5285
Edmundo Pichardo <ce.pichardo@gmail.com>
2018-08-23 10:12:59 -0400

    Create TestingController

--- app/Category.php
+++ app/Category.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
 
-    protected  $fillable =['slug','name','description'];
+    protected  $fillable = ['slug','name','description'];
 
     public function parent()
     {
